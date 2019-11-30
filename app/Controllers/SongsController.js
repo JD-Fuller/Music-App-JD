9246113548c7d2344a9a2c69a96b719ee5caaef4
JD-Fuller <jdfuller@live.com>
2019-11-29 22:18:00 -0700

    posts data from itunes

--- app/Controllers/SongsController.js
+++ app/Controllers/SongsController.js
@@ -4,20 +4,37 @@ import SongService from "../Services/SongsService.js";
 //Private
 /**Draws the Search results to the page */
 function _drawResults() {
-  let template = ''
-  let songs = store.State.songs
-  songs.forEach(song => template += song.Template)
+  let songTemplate = ''
+  let song = store.State.songs
+  song.forEach(song => songTemplate += song.songTemplate)
 
+  document.querySelector("#song-list").innerHTML = songTemplate
+}
+
+
+
+
+
+
+/**Draws the Users saved songs to the page */
+function _drawPlaylist() {
+let template 
 
 }
-/**Draws the Users saved songs to the page */
-function _drawPlaylist() {}
 
 //Public
 export default class SongsController {
   constructor() {
+    console.log("hello from song controller")
+    store.subscribe("songs", _drawResults)
+    _drawResults()
     //TODO Don't forget to register your subscribers
   }
+
+
+
+
+
 
   /**Takes in the form submission event and sends the query to the service */
   search(e) {
@@ -30,11 +47,23 @@ export default class SongsController {
     }
   }
 
+
+
+
+
+
+
+
+
+
+
   /**
    * Takes in a song id and sends it to the service in order to add it to the users playlist
    * @param {string} id
    */
-  addSong(id) {}
+  addSong(id) {
+
+  }
 
   /**
    * Takes in a song id to be removed from the users playlist and sends it to the server
--- app/Models/Song.js
+++ app/Models/Song.js
@@ -2,35 +2,38 @@ export default class Song {
   constructor(data) {
     this.title = data.trackName || data.title;
     this.albumArt =
-      data.albumArt || data.artworkUrl100.replace(/100x100/g, "300x300");
+      data.albumArt || data.artworkUrl100.replace(/100x100/g, "50x50");
     this.artist = data.artistName || data.artist;
     this.album = data.collectionName || data.album;
     this.price = data.trackPrice || data.price;
     this.preview = data.previewUrl || data.preview;
     this._id = data.trackId || data._id;
+    this.durationInMillis = data.durationInMillis || "Unknown";
   }
 
-  get Template() {
+  get songTemplate() {
     return `
-
     <li class="itunes__track">
-                      <img class="itunes__track__cover" src="${this.albumArt}">
+                      <img class="itunes__track__cover" src="${this.albumArt}" style=">
                       <div class="itunes__track__info">
                       <h3 class="itunes__track__title">${this.title}</h3>
                       <span class="itunes__track__artist">${this.artist}</span>
                       </div>
-                      <span class="itunes__track__time">4:34</span>
                       <button type="button" class="btn btn-success" onclick="app.songsController.addSong('${this._id}')">+</button>
-            </div>
-    `
                     </li>
-
         `;
   }
 
   get playlistTemplate() {
     return `
-
+    <li class="itunes__track">
+                      <img class="itunes__track__cover" src="${this.albumArt}" style=">
+                      <div class="itunes__track__info">
+                      <h3 class="itunes__track__title">${this.title}</h3>
+                      <span class="itunes__track__artist">${this.artist}</span>
+                      </div>
+                      <button type="button" class="btn btn-success" onclick="app.songsController.removeSong('${this._id}')">X</button>
+                    </li>
         `;
   }
 }
